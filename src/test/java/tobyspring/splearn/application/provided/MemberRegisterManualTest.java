a740d0bf8a8a96ea2feb292162d5b528d62835a1
ljh <tjsrsdqq4@gmail.com>
2025-08-04 22:27:28 +0900

    Test: MemberRegisterManualTest 추가
    - 회원 등록에 대한 수동 테스트 케이스 작성 (Stub 및 Mockito 기반)
    - MemberRepositoryStub과 EmailSenderStub Stub 클래스 작성
    - 테스트에서 ID와 상태 검증 및 이메일 전송 여부 확인

---
+++ src/test/java/tobyspring/splearn/application/provided/MemberRegisterManualTest.java
@@ -0,0 +1,51 @@
+package tobyspring.splearn.application.provided;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.test.util.ReflectionTestUtils;
+import tobyspring.splearn.application.MemberService;
+import tobyspring.splearn.application.required.EmailSender;
+import tobyspring.splearn.application.required.MemberRepository;
+import tobyspring.splearn.domain.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+
+class MemberRegisterManualTest {
+    @Test
+    void registerTestStub() {
+        MemberService memberService = new MemberService(new MemberRepositoryStub(), new EmailSenderStub(), MemberFIxture.createPasswordEncoder());
+        Member member = memberService.register(MemberFIxture.createMemberRegisterRequest());
+
+        assertThat(member.getId()).isNotNull();
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
+    }
+
+    @Test
+    void registerTestMockito() {
+        EmailSender emailSenderMock = Mockito.mock(EmailSender.class);
+
+        MemberService memberService = new MemberService(new MemberRepositoryStub(), emailSenderMock, MemberFIxture.createPasswordEncoder());
+        Member member = memberService.register(MemberFIxture.createMemberRegisterRequest());
+
+        assertThat(member.getId()).isNotNull();
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
+
+        Mockito.verify(emailSenderMock).send(eq(member.getEmail()), any(), any());
+    }
+
+    static class MemberRepositoryStub implements MemberRepository {
+        @Override
+        public Member save(Member member) {
+            ReflectionTestUtils.setField(member, "id", 1L);
+            return member;
+        }
+    }
+
+    static class EmailSenderStub implements EmailSender {
+        @Override
+        public void send(Email email, String subject, String body) {
+        }
+    }
+}
