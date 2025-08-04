8a67ee0bfbd0c6c4c1023b4c13cffcc8ab863415
ljh <tjsrsdqq4@gmail.com>
2025-08-04 21:15:57 +0900

    Test: MemberRepositoryTest에 중복 이메일 저장 실패 테스트 추가
    - 중복 이메일 저장 시 DataIntegrityViolationException 발생 테스트 추가
    - Member 엔티티에 @NaturalId 및 @NaturalIdCache 어노테이션 적용

--- src/main/java/tobyspring/splearn/domain/Member.java
+++ src/main/java/tobyspring/splearn/domain/Member.java
@@ -5,6 +5,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.hibernate.annotations.NaturalId;
+import org.hibernate.annotations.NaturalIdCache;
 
 import static java.util.Objects.requireNonNull;
 import static org.springframework.util.Assert.state;
@@ -13,6 +15,7 @@ import static org.springframework.util.Assert.state;
 @Getter
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NaturalIdCache
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,6 +29,7 @@ public class Member {
     private String passwordHash;
 
     @Enumerated(EnumType.STRING)
+    @NaturalId
     private MemberStatus status;
 
     public static Member register(MemberRegisterRequest createRequest, PasswordEncoder passwordEncoder) {
--- src/test/java/tobyspring/splearn/application/required/MemberRepositoryTest.java
+++ src/test/java/tobyspring/splearn/application/required/MemberRepositoryTest.java
@@ -3,9 +3,13 @@ package tobyspring.splearn.application.required;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import tobyspring.splearn.domain.Member;
-import tobyspring.splearn.domain.MemberFIxture;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static tobyspring.splearn.domain.MemberFIxture.createMemberRegisterRequest;
+import static tobyspring.splearn.domain.MemberFIxture.createPasswordEncoder;
 
 @DataJpaTest
 class MemberRepositoryTest {
@@ -14,12 +18,25 @@ class MemberRepositoryTest {
 
     @Test
     void 멤버생성() {
-        Member member = Member.register(MemberFIxture.createMemberRegisterRequest(), MemberFIxture.createPasswordEncoder());
+        Member member = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
 
         assertThat(member.getId()).isNull();
 
         memberRepository.save(member);
 
         assertThat(member.getId()).isNotNull();
+    }
+
+    @Test
+    void 중복이메일실패() {
+        Member member = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
+        memberRepository.save(member);
+
+        Member member2 = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
+        memberRepository.save(member);
+
+        assertThatThrownBy(() -> memberRepository.save(member2))
+                .isInstanceOf(DataIntegrityViolationException.class);
+
     }
 }
